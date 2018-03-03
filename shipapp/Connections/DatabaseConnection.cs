cd4889fc8faa9d10e65e62c03cf07d8ed547dd02
Jesse Fender <jf0772256@otc.edu>
2018-03-03 11:24:19 -0600

    Added base table building syntax to the MSSQL side of thinsg... soon to test that it runs with out error.

--- shipapp/Connections/DatabaseConnection.cs
+++ shipapp/Connections/DatabaseConnection.cs
@@ -155,15 +155,29 @@ namespace shipapp.Connections
                     "CREATE TABLE IF NOT EXISTS carriers(carrier_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, carrier_name VARCHAR(50) NOT NULL UNIQUE, carrier_phone_id INT DEFAULT NULL);",
                     "CREATE TABLE IF NOT EXISTS purchase_orders(po_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, po_number VARCHAR(25) DEFAULT NULL,po_package_count INT DEFAULT 0, po_created_on TIMESTAMP DEFAULT 0, po_created_by INT NOT NULL, po_approved_by INT NOT NULL);",
                     "CREATE TABLE IF NOT EXISTS packages(package_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,package_po_id INT DEFAULT NULL, package_carrier_id INT NOT NULL, package_vendor_id INT NOT NULL, package_deliv_to_id INT NOT NULL, package_deliv_by_id INT DEFAULT NULL, package_signed_for_by_id INT DEFAULT NULL, package_tracking_number VARCHAR(50) DEFAULT NULL, package_receive_date DATE DEFAULT NULL, package_deliver_date DATE DEFAULT NULL, package_notes_id INT DEFAULT NULL);",
-                    "CREATE TABLE IF NOT EXISTS email_addresses(email_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, person_id BIGINT NOT NULL, email_address VARCHAR(100) NOT NULL UNIQUE);", "CREATE TABLE IF NOT EXISTS phone_numbers(phone_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, person_id BIGINT NOT NULL, phone_number VARCHAR(20) NOT NULL);",
+                    "CREATE TABLE IF NOT EXISTS email_addresses(email_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, person_id BIGINT NOT NULL, email_address VARCHAR(100) NOT NULL UNIQUE);",
+                    "CREATE TABLE IF NOT EXISTS phone_numbers(phone_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, person_id BIGINT NOT NULL, phone_number VARCHAR(20) NOT NULL);",
                     "CREATE TABLE IF NOT EXISTS physical_addr(address_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, person_id BIGINT NOT NULL, addr_line1 VARCHAR(50) NOT NULL, addr_line2 VARCHAR(50) DEFAULT NULL, addr_city VARCHAR(50) NOT NULL, addr_state VARCHAR(2) NOT NULL, addr_zip VARCHAR(10) NOT NULL, addr_cntry VARCHAR(2) DEFAULT 'US');",
                     "CREATE TABLE IF NOT EXISTS notes(id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT, note_id BIGINT NOT NULL, note_value VARCHAR(5000) NOT NULL);"
                 };
             }
             else if (DBType == SQLHelperClass.DatabaseType.MSSQL)
             {
-                //cmdTxt += "CREATE TABLE IF NOT EXISTS";
-                cmdTxt = new List<string>() { };
+                //"IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE ",
+                cmdTxt = new List<string>(){
+                    //attempt to create the first table as a test;;
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = users)CREATE TABLE users(user_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, user_fname VARCHAR(50) NOT NULL, user_lname VARCHAR(50) NOT NULL, user_name VARCHAR(50) NOT NULL UNIQUE, user_password VARCHAR(50) NOT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE roles(role_id BigINT NOT NULL IDENTITY(1,1) PRIMARY KEY, role_title VARCHAR(50) NOT NULL, CONSTRAINT UC_Roles UNIQUE(role_title));",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE employees(empl_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, empl_fname VARCHAR(50) NOT NULL, empl_lname VARCHAR(50), empl_phone_id INT DEFAULT NULL, empl_addr_id INT DEFAULT NULL, empl_email_id INT DEFAULT NULL, empl_notes_id INT DEFAULT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE vendors(vend_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, vendor_name VARCHAR(50) NOT NULL UNIQUE, vendor_addr_id INT DEFAULT NULL, vendor_poc_name VARCHAR(50) DEFAULT NULL, vendor_phone_id INT DEFAULT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE carriers(carrier_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, carrier_name VARCHAR(50) NOT NULL UNIQUE, carrier_phone_id INT DEFAULT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE purchase_orders(po_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, po_number VARCHAR(25) DEFAULT NULL,po_package_count INT DEFAULT 0, po_created_on TIMESTAMP DEFAULT 0, po_created_by INT NOT NULL, po_approved_by INT NOT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE packages(package_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,package_po_id INT DEFAULT NULL, package_carrier_id INT NOT NULL, package_vendor_id INT NOT NULL, package_deliv_to_id INT NOT NULL, package_deliv_by_id INT DEFAULT NULL, package_signed_for_by_id INT DEFAULT NULL, package_tracking_number VARCHAR(50) DEFAULT NULL, package_receive_date DATE DEFAULT NULL, package_deliver_date DATE DEFAULT NULL, package_notes_id INT DEFAULT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE email_addresses(email_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, person_id BIGINT NOT NULL, email_address VARCHAR(100) NOT NULL, CONSTRAINT UC_Email UNIQUE);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE phone_numbers(phone_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, person_id BIGINT NOT NULL, phone_number VARCHAR(20) NOT NULL);",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE physical_addr(address_id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, person_id BIGINT NOT NULL, addr_line1 VARCHAR(50) NOT NULL, addr_line2 VARCHAR(50) DEFAULT NULL, addr_city VARCHAR(50) NOT NULL, addr_state VARCHAR(2) NOT NULL, addr_zip VARCHAR(10) NOT NULL, addr_cntry VARCHAR(2) DEFAULT 'US');",
+                    "IF NOT EXISTS(SELECT [name] FROM sys.tables WHERE [name] = roles)CREATE TABLE notes(id BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, note_id BIGINT NOT NULL, note_value VARCHAR(5000) NOT NULL);"
+                };
             }
             //out side all conditions available
             else
