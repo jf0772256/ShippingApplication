9a7e3de8fc1a546c23f73df5cdd52364adb34ae8
Jesse Fender <jf0772256@otc.edu>
2018-03-11 13:27:15 -0500

    #5 In Progress, laid foundation class and starting to get ready for putting the code to the program.

---
+++ shipapp/Connections/DataConnections/Classes/EmployeeConnClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shipapp.Models;
+using shipapp.Models.ModelData;
+
+namespace shipapp.Connections.DataConnections.Classes
+{
+    class EmployeeConnClass:DatabaseConnection
+    {
+        public EmployeeConnClass() : base() { }
+
+        public Faculty GetFaculty(long id)
+        {
+            return new Faculty() { };
+        }
+        public void AddFaculty(Faculty f)
+        {
+            //
+        }
+        public void UpdateFaculty(Faculty f)
+        {
+            //
+        }
+        public void GetAllAfaculty()
+        {
+            //
+        }
+    }
+}
