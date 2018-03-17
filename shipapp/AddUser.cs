e939a687a0860041f793e66adaddcc50b73b4093
Kalin Bowden <kalinbowden@gmail.com>
2018-03-16 20:32:50 -0500

    Added data validattion to Add User

--- shipapp/AddUser.cs
+++ shipapp/AddUser.cs
@@ -29,18 +29,91 @@ namespace shipapp
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            Models.User newUser = new Models.User();
-            newUser.Id = long.Parse(txtId.Text);
-            newUser.FirstName = txtFirstName.Text;
-            newUser.LastName = txtLastName.Text;
-            newUser.Level = new Models.ModelData.Role() {Role_id = 1};
-            newUser.Username = txtUsername.Text;
-            newUser.PassWord = txtPassword.Text;
-            newUser.Person_Id = txtBoxPersonId.Text;
+            // Reset
+            ResetError();
 
-            Connections.DataConnections.DataConnectionClass.UserConn.Write1User(newUser);
+            // Test data before writing to the DB
+            if (ValidateData())
+            {
+                Models.User newUser = new Models.User();
+                newUser.Id = long.Parse(txtId.Text);
+                newUser.FirstName = txtFirstName.Text;
+                newUser.LastName = txtLastName.Text;
+                newUser.Level = new Models.ModelData.Role() { Role_id = 1 };
+                newUser.Username = txtUsername.Text;
+                newUser.PassWord = txtPassword.Text;
+                newUser.Person_Id = txtBoxPersonId.Text;
 
-            this.Close();
+                Connections.DataConnections.DataConnectionClass.UserConn.Write1User(newUser);
+
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Reset the back color after errors
+        /// </summary>
+        private void ResetError()
+        {
+            txtFirstName.BackColor = Color.White;
+            txtLastName.BackColor = Color.White;
+            txtId.BackColor = Color.White;
+            txtLevel.BackColor = Color.White;
+            txtUsername.BackColor = Color.White;
+            txtPassword.BackColor = Color.White;
+            txtBoxPersonId.BackColor = Color.White;
+        }
+
+        private bool ValidateData()
+        {
+            // Method level variables
+            bool pass = true;
+            long num0 = 0;
+
+            // Validate Data
+            if (long.TryParse(txtId.Text, out num0))
+            {
+                pass = false;
+                txtId.BackColor = Color.LightPink;
+            }
+
+            if (txtFirstName.Text == "")
+            {
+                pass = false;
+                txtFirstName.BackColor = Color.LightPink;
+            }
+
+            if (txtLastName.Text == "")
+            {
+                pass = false;
+                txtLastName.BackColor = Color.LightPink;
+            }
+
+            if (txtLevel.Text == "")
+            {
+                pass = false;
+                txtLevel.BackColor = Color.LightPink;
+            }
+
+            if (txtUsername.Text == "")
+            {
+                pass = false;
+                txtUsername.BackColor = Color.LightPink;
+            }
+
+            if (txtPassword.Text == "")
+            {
+                pass = false;
+                txtPassword.BackColor = Color.LightPink;
+            }
+
+            if (txtBoxPersonId.Text == "")
+            {
+                pass = false;
+                txtBoxPersonId.BackColor = Color.LightPink;
+            }
+
+            return pass;
         }
     }
 }
