9b7f7e6972b7a3da537fae1c1a9e2a75e7f91901
Jesse Fender <jf0772256@otc.edu>
2018-04-05 17:41:46 -0500

    fixed the wierd issue with manula backup not triggering only during manual db backup

--- shipapp/Settings.Designer.cs
+++ shipapp/Settings.Designer.cs
@@ -255,6 +255,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(94, 28);
             this.comboBox2.TabIndex = 23;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
             // 
             // button2
             // 
@@ -265,6 +266,7 @@
             this.button2.TabIndex = 22;
             this.button2.Text = "Backup";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -275,6 +277,7 @@
             this.button1.TabIndex = 21;
             this.button1.Text = "Restore";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Settings
             // 
