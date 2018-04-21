b85ef21285c17bd40d97784fbe9d7678735368ca
Kalin Bowden <kalinbowden@gmail.com>
2018-04-21 18:29:46 -0500

    Added sub forms

--- shipapp/Forms/Form1.cs
+++ shipapp/Forms/Form1.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using shipapp.Connections.DataConnections;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using shipapp.Connections.DataConnections;
 
 namespace shipapp
 {
